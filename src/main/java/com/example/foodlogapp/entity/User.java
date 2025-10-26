f2468486e4133a11828802a045a09f1e752604a0
Xiaoyi <Xiaoyi.Zhang21@student.xjtlu.edu.cn>
2025-10-25 23:37:33 -0400

    Add api doc content

--- src/main/java/com/example/foodlogapp/entity/User.java
+++ src/main/java/com/example/foodlogapp/entity/User.java
@@ -1,11 +1,13 @@
 package com.example.foodlogapp.entity;
 
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 public class User {
     private Integer id;
     private String name;
     private String email;
+    @JsonIgnore
     private String password;
 }
